9f8fcf176e4cbd537b54e47a80c3f5d6c3b4f1d9
dcfvg <qwsx@dcfvg.com>
2014-02-27 12:51:02 +0100

    clean and renaming

--- js/subsel.js
+++ js/subsel.js
@@ -1,39 +1,28 @@
 $(function() {
 
-      
-  function update_select(img, selection) {
-      var scaleX = 100 / (selection.width || 1);
-      var scaleY = 100 / (selection.height || 1);
-      console.log(selection);
-      console.log($(img).attr("src"));
-      
+  function select_write(img, selection) {
       var posting = $.post(ajax_url, { img:$(img).attr("src"), selection:selection } );
       posting.done(function( data ) {
         console.log(data);
       });
   }
-  
-  function init(){
-
-    
-    selectionFromFiles();
-  }
-  function selectionFromFiles(){
+  function select_load(){
     $( "img.photo" ).each(function() {
-        var posting = $.post(ajax_url, { img:$(this).attr("src"), askParam:true } );
-        var img = $(this);
-        
-        img.imgAreaSelect({ handles: true, onSelectEnd: update_select });
-  
-        posting.done(function( data ) {
-          if(data.hasData) img.imgAreaSelect({ x1: data.sel.x1, y1: data.sel.y1, x2: data.sel.x2, y2: data.sel.y2 });
-        });
+      var posting = $.post(ajax_url, { img:$(this).attr("src"), askParam:true } );
+      var img = $(this);
+
+      img.imgAreaSelect({ handles: true, onSelectEnd: select_write });
+
+      posting.done(function( data ) {
+        if(data.hasData) img.imgAreaSelect({ x1: data.sel.x1, y1: data.sel.y1, x2: data.sel.x2, y2: data.sel.y2 });
+      });
     });
+  };
+  function init(){
+    select_load();
   }
   
   var ajax_url = "call_ajax.php";
-      
-  
-  
   init();
+
 })
